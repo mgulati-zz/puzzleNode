c0086a57206c84737e93f179afc07701b3905b9c
Mayank Gulati <mynk.gulati@gmail.com>
2013-07-13 07:58:48 -0700

    preliminary websockets stuff

--- app.js
+++ app.js
@@ -42,7 +42,9 @@ function goodie (latitude, longitude) {
 }
 
 //the entire database is just javascript variables
-var markers = {}
+var markers = {};
+var names = {};
+var goodies = {};
 
 markers.first = new goodie(37.524975368048196, -122.310791015625);
 markers.first.members.push('Aya', 'Jordan', 'Devon');
@@ -54,30 +56,53 @@ markers.third = new goodie(37.72130604487683, -122.45361328125);
 markers.third.members.push('Jay', 'Jared', 'Mayank', 'sex');
 
 io.sockets.on('connection', function (socket) {
-  console.log(socket.id);
+  
+  socket.on('name', function(userId) {
+    names[socket.id] = userId;
+  })
+
+  socket.on('updateLocation', function (latitude,longitude) {
+    
+    for (goodie in markers) {
+      if (inRange(goodie, req.query.latitude, req.query.longitude))
+          data['enabledGoodie'] = goodie
+  })
+  
+  socket.on('join', function(goodie) {
+    for (i in io.sockets.manager.roomClients[socket.id])
+      if (io.sockets.manager.roomClients[socket.id][i] != "")
+        socket.leave(io.sockets.manager.roomClients[socket.id][i]);
+    
+    socket.join(goodie);
+    goodies[socket.id] = goodie;
+    if (!($.inArray(names[socket.id], markers[goodie].members)))
+      markers[goodie].members.push(names[socket.id]);
+  })
+  
+  socket.on('unlock', function() {
+
+  })
+
+  socket.on('disconnect', function () {
+    if (goodies[socket.id] && markers[goodies[socket.id]].memebrs.indexOf(names[socket.id]) != -1)
+      markers[goodies[socket.id]].memebrs.splice(markers[goodies[socket.id]].memebrs.indexOf(names[socket.id]),1)
+    delete names[socket.id];
+    io.sockets.in(goodies[socket.id]).emit('personLeft', names[socket.id]);
+    delete goodies[socket.id];
+  });
+
 });
+
+var checkRange = 0.03;
+function inRange(goodie, latitude, longitude) {
+  return (Math.abs(markers[goodie]['latitude'] - latitude) < checkRange &&
+          Math.abs(markers[goodie]['longitude'] - longitude) < checkRange)  
+}
 
 //routing, if css and javascript send file, otherwise render the page
 app.get('/', function(req, res, next){
   if (req.params.room && req.params.room.indexOf(".") !== -1) next();
   else res.render('index.html');
-});
-
-app.get('/getGoodies', function(req,res,next){
-  
-  var data = {}
-  data['goodies'] = markers;
-
-  latitude = req.query.latitude
-  longitude = req.query.longitude
-
-  for (goodie in data['goodies']) {
-    if (Math.abs(data.goodies[goodie]['latitude'] - (latitude)) < 0.03 &&
-        Math.abs(data.goodies[goodie]['longitude'] - (longitude)) < 0.03)
-          data['enabledGoodie'] = goodie
-  }
-
-  res.json(data);
 });
 
 server.listen(app.get('port'));
