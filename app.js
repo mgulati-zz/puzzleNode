71eef732c0181979a0b1071bcaaa99badcea1c06
Jared Smith <jared.rit@me.com>
2013-07-13 13:57:05 -0700

    Remove test image.

--- app.js
+++ app.js
@@ -249,9 +249,19 @@ function upload_image(image) {
    
 }
 
-function download_image(url) {
+var fs = require('fs'),
+    request = require('request');
 
-}
+function download(uri, filename) {
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename));
+  });
+};
+
+download('https://www.google.com/images/srpr/logo3w.png', 'google.png');
 
 
 
